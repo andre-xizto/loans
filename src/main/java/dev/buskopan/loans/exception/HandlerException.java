98f9830aca3d91a063ba38f38d92b7d2fd89aa64
= <=>
2024-07-25 10:09:53 -0300

    create handler exception

---
+++ src/main/java/dev/buskopan/loans/exception/HandlerException.java
@@ -0,0 +1,17 @@
+package dev.buskopan.loans.exception;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+public class HandlerException extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatusCode statusCode, WebRequest request) {
+        return super.handleExceptionInternal(ex, body, headers, statusCode, request);
+    }
+}
