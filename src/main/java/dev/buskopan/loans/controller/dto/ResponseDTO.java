b9dde5a04954af53f6cee4f0f4d80fca31273b91
= <=>
2024-07-25 10:15:50 -0300

    remove unsed imports

--- src/main/java/dev/buskopan/loans/controller/dto/ResponseDTO.java
+++ src/main/java/dev/buskopan/loans/controller/dto/ResponseDTO.java
@@ -1,7 +1,5 @@
 package dev.buskopan.loans.controller.dto;
 
-import dev.buskopan.loans.enums.LoanType;
-
 import java.util.List;
 
 public record ResponseDTO (String customer, List<LoanDTO> loans) {
