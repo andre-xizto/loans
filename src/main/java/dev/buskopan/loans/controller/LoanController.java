8269ca2d04eb5d1bbf5e89be448a6bb3506976da
= <=>
2024-07-25 09:15:19 -0300

    create loan controller

---
+++ src/main/java/dev/buskopan/loans/controller/LoanController.java
@@ -0,0 +1,18 @@
+package dev.buskopan.loans.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/loan")
+public class LoanController {
+
+    @PostMapping
+    public ResponseEntity<?> getLoans() {
+        return null;
+    }
+
+}
